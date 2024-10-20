6461368c70263dcd4e14e9b4b8772ebfc791e90d
dipanshu0001 <dipanshugupta1624@gmail.com>
2024-10-20 12:20:19 +0530

    done

--- realtime-code-editor-backend/server.js
+++ realtime-code-editor-backend/server.js
@@ -35,7 +35,7 @@ function getAllConnectedClients(roomId) {
 }
 
 io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
+    console.info('socket connected', socket.id);
 
     socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
         userSocketMap[socket.id] = username;
